a99378285f973bbf0e7ea75db35fa9c7b8566b8d
bogdan22tsb <bogdan22tsb@gmail.com>
2026-07-29 23:42:42 +0300

    nixos config

---
+++ configuration.nix
@@ -0,0 +1,60 @@
+{ config, lib, pkgs, ... }:
+
+{
+  imports =
+    [
+      ./hardware-configuration.nix
+    ];
+
+  boot.loader.grub.enable = true;
+  boot.loader.grub.device = "/dev/sda";
+
+  networking.hostName = "bogdan22tsb";
+  networking.networkmanager.enable = true;
+
+  time.timeZone = "Europe/Bucharest";
+
+  services.udisks2.enable = true;
+  services.gvfs.enable = true;
+  services.displayManager.ly.enable = true;
+  services.xserver = {
+      enable = true;
+      autoRepeatDelay = 200;
+      autoRepeatInterval = 35;
+      windowManager.qtile.enable = true;
+      displayManager.sessionCommands = ''
+        xwallpaper --center ~/walls/rae.png
+        xset r rate 200 35 &
+      '';
+  };
+
+  services.picom.enable = true;
+
+  programs.bash = {
+    promptInit = ''
+      figlet TSB
+      '';
+  };
+
+  users.users.bogdan = {
+    isNormalUser = true;
+    extraGroups = [ "wheel" ];
+    packages = with pkgs; [
+      tree
+    ];
+  };
+
+  programs.firefox.enable = true;
+
+  nixpkgs.config.allowUnfree = true;
+
+  environment.systemPackages = with pkgs; [
+    vim
+    wget
+    git
+    alacritty
+  ];
+
+  nix.settings.experimental-features = [ "nix-command" "flakes" ];
+  system.stateVersion = "26.05";
+}
---
+++ home.nix
@@ -0,0 +1,64 @@
+{ config, pkgs, ... }:
+
+let
+  dotfiles = "${config.home.homeDirectory}/nixos-dotfiles/config";
+  create_symlink = path: config.lib.file.mkOutOfStoreSymlink path;
+
+  # Standard .config/directory
+  configs = {
+    qtile = "qtile";
+    nvim = "nvim";
+    rofi = "rofi";
+    alacritty = "alacritty";
+    picom = "picom";
+    git = "git";
+  };
+in
+
+{
+  home.username = "bogdan";
+  home.homeDirectory = "/home/bogdan";
+  home.stateVersion = "26.05";
+  programs.bash = {
+    enable = true;
+    shellAliases = {
+      btw = "echo i use nixos-btw";
+      nrs = "sudo nixos-rebuild switch --flake ~/nixos-dotfiles#bogdan22tsb";
+      syu = "sudo nixos-rebuild switch --upgrade";
+      fs = "fastfetch";
+      e = "exit";
+      vim = "nvim";
+      sd = "shutdown now";
+      rn = "reboot";
+    };
+    initExtra = ''
+      	  export PS1="\[\e[38;5;75m\]\u@\h \[\e[38;5;113m\]\w \[\e[38;5;189m\]\$ \[\e[0m\]"
+      	'';
+  };
+
+  xdg.configFile = builtins.mapAttrs
+    (name: subpath: {
+      source = create_symlink "${dotfiles}/${subpath}";
+      recursive = true;
+    })
+    configs;
+
+  home.packages = with pkgs; [
+    neovim
+    ripgrep
+    nil
+    nixpkgs-fmt
+    nodejs
+    gcc
+    rofi
+    xwallpaper
+    fastfetch
+    btop
+    ffmpeg
+    pcmanfm
+    discord
+    figlet
+    cmatrix
+    cava 
+  ];
+}
