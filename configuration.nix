3f4ab54572c74d306ddbe39138a518242bc698b1
bogdan22tsb <bogdan22tsb@gmail.com>
2026-08-24 12:19:03 +0300

    fix monitor hz

--- configuration.nix
+++ configuration.nix
@@ -58,6 +58,7 @@
 
     displayManager.sessionCommands = ''
         xrandr --output DisplayPort-1 --mode 3440x1440 --rate 180
+        xrandr --output DisplayPort-2 --mode 3440x1440 --rate 180
         xwallpaper --center ~/walls/raekwon.png
         xset r rate 200 35 &
       '';
